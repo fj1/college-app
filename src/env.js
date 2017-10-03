cf1718543fd17ba56634200c925564d60066a8b4
Daniel Rutledge <drutledge@pros.com>
2017-10-03 14:48:33 -0500

    Added Daniel's API Key

---
+++ src/env.js
@@ -0,0 +1,4 @@
+const private_key="75b32b376baf0f2c854894479ffb168c0caf10b1";
+const public_key="98fef93f6375e8cc75803b36cd70da03";
+
+export {private_key, public_key}
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,13 @@
-import ENV_VARIABLES from "./env.js";
+import {private_key, public_key} from "./env.js";
 
 const utils = {
   generateHash(timestamp) {
     const md5 = require("md5");
-    const private_key = ENV_VARIABLES.private_key;
-    const public_key = ENV_VARIABLES.public_key;
     return md5(`${timestamp}${private_key}${public_key}`);
   },
 
   getPublicKey() {
-    return ENV_VARIABLES.public_key;
+    return public_key;
   }
 };
 
